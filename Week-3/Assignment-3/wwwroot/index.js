a6970a0d92342796ad880a9c6be84cc66e763f44
ray199229 <ray199229@gmail.com>
2023-06-12 15:05:41 +0800

    fix: correct assignments-3

--- Week-3/Assignment-3/wwwroot/index.js
+++ Week-3/Assignment-3/wwwroot/index.js
@@ -1,15 +1,20 @@
 ﻿function calculation() {
   const number = document.querySelector('#input').value
+  const content = document.querySelector('.container')
   const url = `/data?number=${number}`
   const xhttp = new XMLHttpRequest()
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        location.href= url
-      } else {
-        location.href= url
-        console.error('Error: ' + this.status)
-      }
-    }
   xhttp.open("GET", url, true)
+  xhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = this.responseText
+      if(!isNaN(Number(response))) {
+        content.innerHTML = `<p>This calculation result is: <strong>${response}</strong></p>`
+      } else {
+        content.innerHTML = `<p>${response}</p>`
+      }
+    } else {
+      content.innerHTML = `<p>Error! status code: <strong>${this.status}</strong></p>`
+    }
+  }
   xhttp.send()  
 }
